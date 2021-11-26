015b83ade5c212865de8e2579e52f4fbefaef792
mariabenavidesmartines <maria.benavides.martines@insolar.io>
2021-11-26 22:31:44 +0300

    HW3 - add class Main

---
+++ src/test/java/guru/qa/hw3/Main.java
@@ -0,0 +1,12 @@
+package guru.qa.hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        Dacha dacha = new Dacha();
+        dacha.price = 100;
+        dacha.address = "derevnya Gorka, 10";
+        dacha.setAddress();
+        System.out.println("When season is high the price is " + Dacha.figuredPrice("high", dacha.price) +
+                ". For other time the price is " + Dacha.figuredPrice("low", dacha.price));
+    }
+}
