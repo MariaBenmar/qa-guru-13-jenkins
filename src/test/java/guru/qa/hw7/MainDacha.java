7b3d8f7f5e1667d1fa3a5d90c6d259fa52cc3cf2
mariabenavidesmartines <maria.benavides.martines@insolar.io>
2022-01-22 00:33:49 +0300

    HW7  - OwnershipDacha.java, MainDacha.java

---
+++ src/test/java/guru/qa/hw7/MainDacha.java
@@ -0,0 +1,20 @@
+package guru.qa.hw7;
+
+
+public class MainDacha {
+
+    public static void main(String[] args) {
+        OwnershipDacha dachaGorka = new OwnershipDacha(10000, "derevnya Gorka, 10", true, new OwnershipDacha.Ground(12, 500, "Kingisepp area"));
+
+        System.out.println("The price of the dacha is " + dachaGorka.getPrice());
+        System.out.println("The address of the dacha is : " + dachaGorka.getAddress());
+        System.out.println("Availability for rent: " + dachaGorka.isAvailableRent());
+
+        dachaGorka.ground.groundPriceCalculated();
+        dachaGorka.ground.setNumberOfAcres(15);
+        dachaGorka.ground.groundPriceCalculated();
+
+        System.out.println("When season is high the price fo RENT is " + OwnershipDacha.dachaPriceForSeason("high", dachaGorka.price) +
+                ". For other time the price for RENT is " + OwnershipDacha.dachaPriceForSeason("low", dachaGorka.price));
+    }
+}
---
+++ src/test/java/guru/qa/hw7/OwnershipDacha.java
@@ -0,0 +1,102 @@
+package guru.qa.hw7;
+
+public class OwnershipDacha {
+    int price;
+    String address;
+    boolean availableRent;
+    Ground ground;
+    public static boolean countMethod;
+
+    public OwnershipDacha(int price, String address, boolean availableRent, Ground ground) {
+        this.price = price;
+        this.address = address;
+        this.availableRent = availableRent;
+        this.ground = ground;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setAvailableRent(boolean availableRent) {
+        this.availableRent = availableRent;
+    }
+
+    public void setGround(Ground ground) {
+        this.ground = ground;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public boolean isAvailableRent() {
+        return availableRent;
+    }
+
+    public Ground getGround() {
+        return ground;
+    }
+
+    public static int dachaPriceForSeason(String season, int priceRent) {
+        if (season.equals("high"))
+            return priceRent * 2;
+        return priceRent;
+    }
+
+    static class Ground {
+        int numberOfAcres;
+        int priceForAcr;
+        String department;
+
+        public Ground(int numberOfAcres, int priceForAcr, String department) {
+            this.numberOfAcres = numberOfAcres;
+            this.priceForAcr = priceForAcr;
+            this.department = department;
+        }
+
+        public void setNumberOfAcres(int numberOfAcres) {
+            this.numberOfAcres = numberOfAcres;
+        }
+
+        public void setPriceForAcr(int priceForAcr) {
+            this.priceForAcr = priceForAcr;
+        }
+
+        public void setDepartment(String department) {
+            this.department = department;
+        }
+
+        public int getNumberOfAcres() {
+            return numberOfAcres;
+        }
+
+        public int getPriceForAcr() {
+            return priceForAcr;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+        public void groundPriceCalculated() {
+
+            if (!countMethod) {
+                System.out.println("There current cost of the ground: " + getNumberOfAcres() * getPriceForAcr());
+            } else {
+
+                System.out.println("There UPDATED cost of the ground: " + getNumberOfAcres() * getPriceForAcr());
+            }
+            countMethod = !countMethod;
+
+        }
+    }
+}
